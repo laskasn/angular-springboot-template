55f7fe802e433c3859925728a1a27abdac8ef703
Nikolas Laskaris <laskarisn@gmail.com>
2018-12-20 10:39:56 +0200

    Commit template files

---
+++ route-visualiser/src/main/java/gr/grnet/mw/bus/visualisation/routevisualiser/RouteVisualiserApplication.java
@@ -0,0 +1,14 @@
+package gr.grnet.mw.bus.visualisation.routevisualiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteVisualiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteVisualiserApplication.class, args);
+	}
+
+}
+
---
+++ route-visualiser/src/test/java/gr/grnet/mw/bus/visualisation/routevisualiser/RouteVisualiserApplicationTests.java
@@ -0,0 +1,17 @@
+package gr.grnet.mw.bus.visualisation.routevisualiser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RouteVisualiserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
